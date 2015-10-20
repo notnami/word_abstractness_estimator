47a635a2d4e607dc994d2eb7ed7121cda72d1e06
Anton Osten <anton@ostensible.me>
2015-10-19 21:21:38 -0500

    fixed code for current NLTK WordNet API and added console input to query words
    Python 3

--- abstractness_estimater.py
+++ abstractness_estimater.py
@@ -18,7 +18,7 @@ class AbstractnessEstimator:
         abstractness_list = []
         for lemma in lemmas:
             # find all the hypernyms
-            tree = lemma.synset.tree(lambda s:s.hypernyms())
+            tree = lemma.synset().tree(lambda s:s.hypernyms())
             hypernyms = self._flatten(tree)
 
             # count physical_entity and abstraction synsets
@@ -38,7 +38,8 @@ class AbstractnessEstimator:
 
     # count the number of synsets that has a name
     def _count_synset(self, synsets, name):
-        return len([s for s in synsets if s.name.startswith(name)])
+        count = sum(1 for s in synsets if s.name().startswith(name))
+        return count
     
     # flatten a list
     def _flatten(self, l):
@@ -51,22 +52,10 @@ class AbstractnessEstimator:
             return [l]
 
 if __name__ == '__main__':
-    print '=Usage example='
-    print
     ae = AbstractnessEstimator()
-
-    print 'Concrete examples'
-    for word in ['dog', 'cat', 'cake']:
-        print '', word, ae.estimate(word)
-
-    print 'Semi-abstract examples'
-    for word in ['spring', 'mountain', 'line']:
-        print '', word, ae.estimate(word)
-
-    print 'Abstract examples'
-    for word in ['peace', 'justice', 'freedom']:
-        print '', word, ae.estimate(word)
-
-    print 'NOTE: unknown words are considered concrete'
-    for word in ['hoge', 'dsfijf', '!!??']:
-        print '', word, ae.estimate(word)
+    print('enter a word to see its abstractness')
+    print('NOTE: unknown words are considered concrete')
+    while True:
+        word = input('> ')
+        result = ae.estimate(word.strip())
+        print(result)
